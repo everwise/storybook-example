2acdfd6c16c3bfe1a44831effe9b19df90756e9d
christiancho <christian@geteverwise.com>
2017-09-28 11:12:00 -0400

    Update presentation and create PDF version

--- stories/index.js
+++ stories/index.js
@@ -52,96 +52,7 @@ storiesOf('Buttons', module)
         ))}
       </tbody>
     </Table>
-  ))
-  .addWithJSX('ButtonGroup', () => (
-    <ButtonGroup
-      bsSize={select('bsSize', sizes)}
-      vertical={boolean('vertical', false)}
-      justified={boolean('justified', false)} // 
-    >
-      <Button>Left</Button>
-      <Button>Middle</Button>
-      <Button>Right</Button>
-    </ButtonGroup>
-  ))
-  .addWithJSX('ButtonToolbar', () => (
-    <ButtonToolbar>
-      <ButtonGroup bsSize={select('bsSize', sizes, 'md')}>
-        <Button>1</Button>
-        <Button>2</Button>
-        <Button>3</Button>
-        <Button>4</Button>
-      </ButtonGroup>
-      <ButtonGroup bsSize={select('bsSize', sizes, 'md')}>
-        <Button>5</Button>
-        <Button>6</Button>
-        <Button>7</Button>
-      </ButtonGroup>
-      <ButtonGroup bsSize={select('bsSize', sizes, 'md')}>
-        <Button>8</Button>
-      </ButtonGroup>
-    </ButtonToolbar>
   ));
-
-const placements = ['top', 'right', 'bottom', 'left'];
-const tooltip = (
-  <Tooltip id="tooltip">
-    Check this info.
-  </Tooltip>
-);
-
-storiesOf('Overlays', module)
-  .addWithJSX('Tooltip', () => (
-    <Tooltip
-      id="tooltip-1"
-      placement={select('placement', placements, 'right')}
-      className="in"
-    >
-      {text('text', 'This is a tooltip.')}
-    </Tooltip>
-  ))
-  .addWithJSX('with OverlayTrigger', () => (
-    <OverlayTrigger
-      placement={select('placement', placements, 'right')}
-      overlay={tooltip}
-      rootClose={boolean('rootClose', false)}
-    >
-      <Button bsStyle="default">Holy guacamole!</Button>
-    </OverlayTrigger>
-  ));
-
-const navStyles = ['pills', 'tabs'];
-
-storiesOf('Navs', module)
-  .addWithJSX('Nav', () => (
-    <Nav
-      bsStyle={select('bsStyle', navStyles, 'pills')}
-      activeKey={1}
-      onSelect={action('select')}
-      stacked={boolean('stacked', false)}
-    >
-      <NavItem
-        eventKey={1}
-        href="/home"
-      >
-        NavItem 1 content
-      </NavItem>
-      <NavItem
-        eventKey={2}
-        title="Item"
-      >
-        NavItem 2 content
-      </NavItem>
-      <NavItem
-        eventKey={3}
-        disabled={boolean('disable item?', true)}
-      >
-        NavItem 3 content
-      </NavItem>
-    </Nav>
-  ));
-
-const panelStyles = ['primary', 'success', 'info', 'warning', 'danger', undefined];
 
 storiesOf('Page layout', module)
   .addWithJSX('Table', () => (
@@ -179,16 +90,4 @@ storiesOf('Page layout', module)
         </tr>
       </tbody>
     </Table>
-  ))
-  .addWithJSX('Panel', () => (
-    <Panel
-      bsStyle={select('bsStyle', panelStyles)}
-      header={text('header')}
-      footer={text('footer')}
-      collapsible={boolean('collapsible', false)}
-    >
-      This is a panel
-    </Panel>
   ));
-
-  // TODO: Add another bootstrap component that we will demonstrate with
